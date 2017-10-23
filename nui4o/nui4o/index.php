8c6d598af4c694eb2ddee28034984b05754e11bf
makemeagain <30768701+makemeagain@users.noreply.github.com>
2017-10-23 11:48:34 +0500

    Add files via upload

---
+++ nui4o/nui4o/index.php
@@ -0,0 +1,43 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Бально-рейтинговая система 1.0</title>
+
+<meta name="description" content="Директолог+"/>
+<meta name="keywords" content=""/>
+<meta name="author" content="Ну и 4о">
+<meta name="GENERATOR" content="Текстовый редактор">
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<script src="../js/jquery-3.1.1.js"></script>
+<link rel="icon" href="favicon.ico" />
+<link rel="shortcut icon" href="favicon.ico" />
+
+<link href="css/st.css" rel="stylesheet">
+
+<script src="js/jquery.maskedinput.js"></script>
+<script type="text/javascript" charset="utf-8" src="../js/jquery.tubular.1.0.js"></script>
+				<script type="text/javascript">
+					jQuery(function($){
+					   $('#phone').mask("+7 (999) 999-9999");
+					   $('#phoneback').mask("+7 (999) 999-9999");
+					});
+				</script>
+
+<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter45894603 = new Ya.Metrika({ id:45894603, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = false; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <!-- /Yandex.Metrika counter -->
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
+</head>
+
+<body>	
+	<form method="post" class="forma">
+			<input name="login" type="text" placeholder="Логин">
+			<input name="passwrd" type="text" placeholder="Пароль">
+			<button type="submit" name="buton">Войти</button>
+			<button type="submit" name="buton">Восстановить пароль</button>
+	</form>
+</body>
+</html>
